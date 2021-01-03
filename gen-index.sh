98de10bd49c274a20ab1d7b04197317dd244566c
jholty66 <burholtcharliegmail.com>
2021-01-03 15:33:02 +0000

    fix style

--- gen-index.sh
+++ gen-index.sh
@@ -1,19 +1,29 @@
 #!/bin/sh
-ls *html
-echo '<!doctype html>
+DIR=$(pwd)
+echo $DIR
+for d in $(find . -not -path '*/\.*' -type d); do
+	echo;echo $d;cd $d;STYLE=$(realpath --relative-to=. $DIR);STYLE="${STYLE}/style.css";echo $STYLE
+	echo "<!doctype html>
 <html>
 	<head>
-		<link rel="stylesheet" href="jh___.css" /link>
+		<link rel=\"stylesheet\" href=\"$STYLE\" /link>
 		<title>Index</title>
 	</head>
-	<body>' > index.html
-for file in $(ls *html); do
-	[ $file = "index.html" ] || sed -n "s/[\t]*<title>\(.*\)<\/title>/		<a href=$file>\1<\/a>/Ip" $file >> index.html
-done
-echo '		<p>Directories</p>' >> index.html
-for dir in $(ls -d */);do
-	echo "		<a href=$dir>$dir</a>" >> index.html
-done
-echo '	</body>
+	<body>
+		<p>HTML</p>" > index.html
+	for html in $(ls *html); do
+		[ $html = "index.html" ] || sed -n "s/[\t]*<title>\(.*\)<\/title>/		<a href=$html>\1<\/a><br>/Ip" $html >> index.html
+	done
+	echo '		<p>PDF</p>' >> index.html
+	for pdf in $(ls *pdf); do
+		echo "		<a href=\"$pdf\">$pdf</a><br>" >> index.html
+	done
+	echo '		<p>Directories</p>' >> index.html
+	for dir in $(ls -d */);do
+		echo "		<a href=${dir}index.html>$dir</a>" >> index.html
+	done
+	echo '	</body>
 </html>' >> index.html
-cat index.html
+	cat index.html
+done
+
--- publish.sh
+++ publish.sh
@@ -1,7 +1,7 @@
 cd $(dirname "$0")
 pwd
 ./gen-index.sh
-git add *html
+git add *html *pdf
 git commit -m $(date +%Y-%m-%d)--sync-website
 git diff *html
 git push
