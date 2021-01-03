ea1524d7d2444fa6b050f70d7b5106f55d5bde40
jholty66 <burholtcharliegmail.com>
2021-01-03 13:02:02 +0000

    tidy script

--- gen-index.sh
+++ gen-index.sh
@@ -7,11 +7,7 @@ echo '<!doctype html>
 		<title>Index</title>
 	</head>
 	<body>' > index.html
-
-for file in $(ls *html)
-do
-[ $file = "index.html" ] || sed -n "s/[\t]*<title>\(.*\)<\/title>/		<a href=$file>\1<\/a>/Ip" $file >> index.html
-done
+for file in $(ls *html);do;[ $file = "index.html" ] || sed -n "s/[\t]*<title>\(.*\)<\/title>/		<a href=$file>\1<\/a>/Ip" $file >> index.html;done
 echo '	</body>
 </html>' >> index.html
 cat index.html
